53f8e2e18f51384fa88e068d0524460baa370d4a
juanrv <juan.reyes@pitcher.com.au>
2015-10-19 15:43:00 +1100

    Finishing off making the app isomorphic - Including use of babel

--- server/main.js
+++ server/main.js
@@ -4,19 +4,19 @@ var parser = require('body-parser');
 var React = require('react/addons');
 var GroceryItem = require('./models/GroceryItem.js');
 
+require('babel/register');
 require('./database.js');
 
 app.get('/', function(req, res) {
-  res.render('./../app/index.ejs', {});
-  /*var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+  //res.render('./../app/index.ejs', {});
+  var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
 
   GroceryItem.find(function(error, doc) {
     var generated = React.renderToString(application({
       items: doc
     }));
+    res.render('./../app/index.ejs', { reactOutput: generated });
   });
-
-  res.render('./../app/index.ejs', { reactOutput: generated });*/
 })
 .use(express.static(__dirname + '/../.tmp'))
 .use('/bower_components',express.static(__dirname + '/../bower_components'))
